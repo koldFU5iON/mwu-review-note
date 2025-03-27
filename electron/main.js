6b19b7207a1d6f78ac700f4b454b699cd6791e75
Devon Stanton <devon.stanton@gmail.com>
2025-03-27 23:28:31 +0100

    added in sidebar and mock data for folder/file structure

--- electron/main.js
+++ electron/main.js
@@ -1,20 +1,20 @@
-import { app, BrowserWindow, globalShortcut } from 'electron'
-import path from 'path'
-import { fileURLToPath } from 'url';
+import { app, BrowserWindow, globalShortcut } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import { ipcMain } from 'electron';
-import { saveNote } from './utils/saveNote.js';
+import { ipcMain } from "electron";
+import { saveNote } from "./utils/saveNote.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let mainWindow;
 
-ipcMain.on('save-note', (event, data) => {
+ipcMain.on("save-note", (event, data) => {
   saveNote(data);
 });
 
-console.log('🔍 preload path:', path.join(__dirname, 'preload.js'));
+console.log("🔍 preload path:", path.join(__dirname, "preload.js"));
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -22,19 +22,19 @@ function createWindow() {
     height: 600,
     alwaysOnTop: false,
     autoHideMenuBar: true,
+    openDevTools: false,
     show: true,
     webPreferences: {
       contextIsolation: true,
-      nodeIntegration: false, 
-      preload: path.join(__dirname, 'preload.js'),
-    }
-    
+      nodeIntegration: false,
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   if (app.isPackaged) {
-    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+    mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
   } else {
-    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.loadURL("http://localhost:5173");
     mainWindow.webContents.openDevTools();
   }
 }
@@ -43,8 +43,8 @@ app.whenReady().then(() => {
   createWindow();
 
   // Register global hotkey
-  globalShortcut.register('Ctrl+Alt+Space', () => {
-    console.log('🔥 Ctrl+Shift+Space pressed!');
+  globalShortcut.register("Ctrl+Alt+Space", () => {
+    console.log("🔥 Ctrl+Shift+Space pressed!");
     if (mainWindow) {
       if (mainWindow.isVisible()) {
         mainWindow.hide();
@@ -56,10 +56,10 @@ app.whenReady().then(() => {
 });
 
 // Clean up global shortcuts on exit
-app.on('will-quit', () => {
+app.on("will-quit", () => {
   globalShortcut.unregisterAll();
 });
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
