7aa064c513df372e505936e821a0cc2e35d18084
koldFU5iON <devon.stanton@gmail.com>
2025-03-28 09:40:02 +0100

    updated the port

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5174, // or any free port you want
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
